bdf20e4da87fbc60b2e8444efdc54bc809af9c6c
Dzheylyan Ademova <djeni.ademova@gmail.com>
2021-12-13 21:59:14 +0200

    Add Day 12 and 13 solutions. Day 12, task 2 needs heavy rework.

---
+++ Day12/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day12
+{
+    //public struct Line
+    //{
+    //    public string Start { get; set; }
+    //    public string End { get; set; }
+    //}
+
+    public struct Point
+    {
+        public string Name { get; set; }
+
+        public List<string> Siblings { get; set; }
+    }
+
+    class Program
+    {
+        //public static List<Line> InputLines = new List<Line>();
+        public static List<Point> InputPoints = new List<Point>();
+
+        static void Main(string[] args)
+        {
+            ReadInput();
+            Console.WriteLine("Final result TaskOne = " + TaskOne());
+
+            // hella slow - takes ~70min to find final answer, rework to be fast after finding pattern in the data
+            Console.WriteLine("Final result TaskTwo = " + TaskTwo());
+        }
+
+        private static long TaskOne()
+        {
+            long result = 0;
+            List<List<Point>> possiblePaths = new List<List<Point>>();
+
+            IteratePaths(possiblePaths, new List<Point>(), InputPoints.FirstOrDefault(p => p.Name == "start"));
+
+            foreach (var path in possiblePaths)
+            {
+                Console.WriteLine(string.Join(",", path.Select(p => p.Name)));
+            }
+
+            Console.WriteLine("Total count = " + possiblePaths.Count);
+
+            //foreach (var path in possiblePaths)
+            //{
+            //    var currentPath = path;
+            //    bool visitsSmallCaveAtMostOnce = currentPath.Where(p => IsSmallCaveAndNotEnd(p.Name)).Count() == 1;
+
+            //    if (visitsSmallCaveAtMostOnce)
+            //    {
+            //        result += 1;
+            //    }
+            //}
+
+            result = possiblePaths.Count;
+
+            return result;
+        }
+
+        private static long TaskTwo()
+        {
+            long result = 0;
+            List<List<Point>> possiblePaths = new List<List<Point>>();
+
+            IteratePaths(possiblePaths, new List<Point>(), InputPoints.FirstOrDefault(p => p.Name == "start"), true);
+
+            foreach (var path in possiblePaths)
+            {
+                Console.WriteLine(string.Join(",", path.Select(p => p.Name)));
+            }
+
+            Console.WriteLine("Total count = " + possiblePaths.Count);
+
+            result = possiblePaths.Count;
+
+            return result;
+        }
+
+        private static bool IsSmallCaveAndNotEnd(string name)
+        {
+            return name != "end" && name != "start" && name.All(c => char.IsLower(c));
+        }
+
+        private static void IteratePaths(List<List<Point>> paths, List<Point> currentPath, Point currentPoint, bool canVisitSmallCaveTwiceOnlyOnce = false)
+        {
+            // add current point
+            var newCurrentPath = new List<Point>();
+            newCurrentPath.AddRange(currentPath);
+            newCurrentPath.Add(currentPoint);
+
+            if (canVisitSmallCaveTwiceOnlyOnce && IsSmallCaveAndNotEnd(currentPoint.Name))
+            {
+                var twiceSmallCaveAlreadyUsed = newCurrentPath.Where(p => p.Name == currentPoint.Name).Count() > 1;
+
+                if (twiceSmallCaveAlreadyUsed)
+                {
+                    canVisitSmallCaveTwiceOnlyOnce = false;
+                }
+            }
+
+            // get next possible points
+            var nextPoints = GetNextPossibleStep(newCurrentPath, currentPoint, canVisitSmallCaveTwiceOnlyOnce);
+
+            // if no next points, check if current is "end" point & if you should add it to paths
+            if (nextPoints.Count == 0)
+            {
+                if (currentPoint.Name == "end")
+                {
+                    string joinedCurrentPath = string.Join(",", newCurrentPath.Select(p => p.Name));
+                    bool currentPathExistInPaths = paths.FirstOrDefault(p => string.Join(",", p.Select(x => x.Name)) == joinedCurrentPath) != null;
+
+                    if (!currentPathExistInPaths)
+                    {
+                        paths.Add(newCurrentPath);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+
+            }
+
+            foreach (var nextPt in nextPoints.OrderBy(x => x.Name))
+            {
+                IteratePaths(paths, newCurrentPath, nextPt, canVisitSmallCaveTwiceOnlyOnce);
+            }
+        }
+
+        private static List<Point> GetNextPossibleStep(List<Point> currentPath, Point currentPoint, bool canVisitSmallCaveTwice = false)
+        {
+            var result = new List<Point>();
+            //canVisitSmallCaveTwice = canVisitSmallCaveTwice ? true : false;
+            if (currentPoint.Name == "end")
+            {
+                return result;
+            }
+
+            foreach (var ptName in currentPoint.Siblings)
+            {
+                if (ptName == "start")
+                {
+                    continue;
+                }
+                if (ptName.All(c => char.IsLower(c)) && canVisitSmallCaveTwice && !currentPath.All(p => p.Name != ptName))
+                {
+                    result.Add(InputPoints.FirstOrDefault(p => p.Name == ptName));
+                }
+                else if (ptName.All(c => char.IsLower(c)) && currentPath.All(p => p.Name != ptName))
+                {
+                    result.Add(InputPoints.FirstOrDefault(p => p.Name == ptName));
+                }
+                else if (ptName.All(c => char.IsUpper(c)))
+                {
+                    result.Add(InputPoints.FirstOrDefault(p => p.Name == ptName));
+                }
+            }
+
+            return result;
+        }
+
+        //private static List<Line> GetAllPossibleLineDirections()
+        //{
+        //    List<Line> lines = new List<Line>();
+
+        //    for (int i = 0; i < InputLines.Count; i++)
+        //    {
+        //        Line currentLine = InputLines[i];
+        //        lines.Add(new Line() { Start = currentLine.Start, End = currentLine.End });
+
+        //        if (currentLine.Start != "start" && currentLine.End != "start" && currentLine.End != "end" && currentLine.Start != "start")
+        //        {
+        //            if (currentLine.Start.All(c => char.IsLower(c)) && currentLine.End.All(c => char.IsLower(c)))
+        //            {
+        //                continue;
+        //            }
+        //            else
+        //            {
+        //                lines.Add(new Line { Start = currentLine.End, End = currentLine.Start });
+        //            }
+        //        }
+        //    }
+
+        //    return lines;
+        //}
+
+        //private static void ReadInput()
+        //{
+        //    string inputLine = Console.ReadLine();
+
+        //    while (!string.IsNullOrWhiteSpace(inputLine))
+        //    {
+        //        string[] currentPathLine = inputLine
+        //            .Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+        //        InputLines.Add(new Line() { Start = currentPathLine[0].Trim(), End = currentPathLine[1].Trim() });
+
+        //        inputLine = Console.ReadLine();
+        //    }
+        //}
+
+        private static void ReadInput()
+        {
+            string inputLine = Console.ReadLine();
+
+            while (!string.IsNullOrWhiteSpace(inputLine))
+            {
+                string[] currentPathLine = inputLine
+                    .Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var startPoint = new Point() { Name = currentPathLine[0] };
+                var endPoint = new Point() { Name = currentPathLine[1] };
+
+                if (InputPoints.Any(p => p.Name == startPoint.Name))
+                {
+                    var existingPoint = InputPoints.FirstOrDefault(p => p.Name == startPoint.Name);
+                    if (!existingPoint.Siblings.Contains(endPoint.Name))
+                    {
+                        existingPoint.Siblings.Add(endPoint.Name);
+                    }
+                }
+                else
+                {
+                    startPoint.Siblings = endPoint.Name == "start" ? new List<string>() : new List<string>() { endPoint.Name };
+                    InputPoints.Add(startPoint);
+                }
+
+                if (InputPoints.Any(p => p.Name == endPoint.Name))
+                {
+                    var existingPoint = InputPoints.FirstOrDefault(p => p.Name == endPoint.Name);
+                    if (!existingPoint.Siblings.Contains(startPoint.Name))
+                    {
+                        existingPoint.Siblings.Add(startPoint.Name);
+                    }
+                }
+                else
+                {
+                    endPoint.Siblings = startPoint.Name == "start" ? new List<string>() : new List<string>() { startPoint.Name };
+                    InputPoints.Add(endPoint);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Day13/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day13
+{
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    class Program
+    {
+        public static List<Point> InputPoints = new List<Point>();
+
+        public static List<string> Folds = new List<string>();
+
+        static void Main(string[] args)
+        {
+            ReadInput();
+            Console.WriteLine("Final result TaskOne = " + TaskOne());
+            Console.WriteLine("Final result TaskTwo = ");
+            TaskTwo();
+        }
+
+        private static long TaskOne()
+        {
+            long result = 0;
+            int maxX = InputPoints.Select(p => p.X).Max(x => x);
+            int maxY = InputPoints.Select(p => p.Y).Max(x => x);
+
+            List<int[]> matrix = new List<int[]>();
+
+            for (int i = 0; i < maxY + 1; i++)
+            {
+                matrix.Add(new int[maxX + 1]);
+            }
+
+            MarkPoints(matrix, InputPoints);
+
+            foreach (string foldCmd in Folds.Take(1))
+            {
+                char direction = foldCmd[0];
+                int index = int.Parse(foldCmd.Substring(2).Trim());
+                matrix = ProcessFold(matrix, direction, index);
+            }
+
+            result = GetCountOfMarkedPointsInMatrix(matrix);
+
+            return result;
+        }
+
+        private static void TaskTwo()
+        {
+            int maxX = InputPoints.Select(p => p.X).Max(x => x);
+            int maxY = InputPoints.Select(p => p.Y).Max(x => x);
+
+            List<int[]> matrix = new List<int[]>();
+
+            for (int i = 0; i < maxY + 1; i++)
+            {
+                matrix.Add(new int[maxX + 1]);
+            }
+
+            MarkPoints(matrix, InputPoints);
+
+            foreach (string foldCmd in Folds)
+            {
+                char direction = foldCmd[0];
+                int index = int.Parse(foldCmd.Substring(2).Trim());
+                matrix = ProcessFold(matrix, direction, index);
+            }
+
+
+            // task answer is visualised with the following:
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        Console.Write("   ");
+                    }
+                    else
+                    {
+                        Console.Write(" " + matrix[i][j] + " ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ResetColor();
+        }
+
+        private static List<int[]> ProcessFold(List<int[]> matrix, char direction, int index)
+        {
+            List<int[]> foldedMatrix = new List<int[]>();
+            int newMatrixXLen = direction == 'x' ? index : matrix[0].Length;
+            int newMatrixYLen = direction == 'y' ? index : matrix.Count;
+
+            for (int i = 0; i < newMatrixYLen; i++)
+            {
+                foldedMatrix.Add(new int[newMatrixXLen]);
+
+                // copy existing points in the fold
+                for (int j = 0; j < newMatrixXLen; j++)
+                {
+                    foldedMatrix[i][j] = matrix[i][j];
+                }
+            }
+
+            // get new points
+            List<Point> foldedPoints = GetFoldedPoints(matrix, direction, index);
+
+            MarkPoints(foldedMatrix, foldedPoints);
+
+            return foldedMatrix;
+        }
+
+        private static List<Point> GetFoldedPoints(List<int[]> matrix, char direction, int index)
+        {
+            List<Point> result = new List<Point>();
+            int startX = direction == 'x' ? index + 1 : 0;
+            int endX = matrix[0].Length;
+            int startY = direction == 'y' ? index + 1 : 0;
+            int endY = matrix.Count();
+
+            for (int i = startY; i < endY; i++)
+            {
+                for (int j = startX; j < endX; j++)
+                {
+                    if (matrix[i][j] != 0)
+                    {
+                        var foldedPoint = new Point()
+                        {
+                            X = direction == 'x' ? (endX - 1) - j : j,
+                            Y = direction == 'y' ? (endY - 1) - i : i
+                        };
+
+                        result.Add(foldedPoint);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static long GetCountOfMarkedPointsInMatrix(List<int[]> matrix)
+        {
+            long result = 0;
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                result += matrix[i].Sum(x => x);
+            }
+
+            return result;
+        }
+
+        private static void MarkPoints(List<int[]> matrix, List<Point> points)
+        {
+            foreach (Point pt in points)
+            {
+                matrix[pt.Y][pt.X] = 1;
+            }
+        }
+
+        private static void ReadInput()
+        {
+            string inputLine = Console.ReadLine();
+
+            while (!string.IsNullOrWhiteSpace(inputLine))
+            {
+                int[] currentPoint = inputLine
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x.Trim()))
+                    .ToArray();
+
+                InputPoints.Add(new Point() { X = currentPoint[0], Y = currentPoint[1] });
+
+                inputLine = Console.ReadLine();
+            }
+
+            inputLine = Console.ReadLine();
+
+            while (!string.IsNullOrWhiteSpace(inputLine))
+            {
+                string currentFold = inputLine
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Last();
+
+                Folds.Add(currentFold);
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
